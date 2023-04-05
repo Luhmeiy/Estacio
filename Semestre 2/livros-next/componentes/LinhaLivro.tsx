fe704fd8c2cf6b8de659d4bb54056983839c2547
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-04-05 19:59:23 -0300

    Add navbar

---
+++ Semestre 2/livros-next/componentes/LinhaLivro.tsx
@@ -0,0 +1,40 @@
+import { ControleEditora } from "@/classes/controle/ControleEditora";
+import { Livro } from "@/classes/modelo/Livro";
+
+interface LinhaLivroProps {
+	livro: Livro;
+	excluir(id: number): void;
+}
+
+const controleEditora = new ControleEditora();
+
+export const LinhaLivro: React.FC<LinhaLivroProps> = (props) => {
+	const nomeEditora = controleEditora.getNomeEditora(props.livro.codEditora);
+
+	return (
+		<tr>
+			<td>
+				{props.livro.titulo}
+				<br />
+				<button
+					type="button"
+					className="btn btn-danger"
+					onClick={() => props.excluir(props.livro.codigo)}
+				>
+					Excluir
+				</button>
+			</td>
+
+			<td>{props.livro.resumo}</td>
+			<td>{nomeEditora}</td>
+
+			<td>
+				<ul>
+					{props.livro.autores.map((autor, index) => {
+						return <li key={index}>{autor}</li>;
+					})}
+				</ul>
+			</td>
+		</tr>
+	);
+};
